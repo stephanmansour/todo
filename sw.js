de9d3d7b5f7e333da7c83327eb9aed156cd906c8
stephanmansour <5vtqzptn9c@privaterelay.appleid.com>
2026-07-27 11:40:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Hält die App offline lauffähig. Aufgaben liegen nicht hier drin,
    sondern im Speicher des Browsers - dieser Cache enthält nur die Hülle. */
 
-var CACHE = 'todo-v1';
+var CACHE = 'todo-v2';
 
 var SHELL = [
   './',
